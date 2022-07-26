74cdc5dda3ca4362b9e99810f09e91e3a1495909
sougata das <sougata19582001@gmail.com>
2022-07-27 01:04:05 +0530

    formatting

--- generateparenthesis.py
+++ generateparenthesis.py
@@ -1,17 +1,18 @@
 def generate_paranthesis(n, s=[], opening=0, closing=0, ans=[]):
     if len(s) == 2*n:
         ans.append("".join(s))
-    
+
     if opening < n:
         s.append("(")
         generate_paranthesis(n, s, opening+1, closing, ans)
         s.pop()
-    
+
     if closing < opening:
         s.append(")")
         generate_paranthesis(n, s, opening, closing+1, ans)
         s.pop()
-    
+
     return ans
+
 
 print(generate_paranthesis(3))
--- permutationofgivenstring.py
+++ permutationofgivenstring.py
@@ -5,10 +5,11 @@ def find_all_permutation(string):
     permute(a, 0, r, ans)
     return ans
 
+
 def permute(a, l, r, ans):
     if l == r:
         ans.append("".join(a))
-    
+
     for i in range(l, r):
         a[i], a[l] = a[l], a[i]
         permute(a, l+1, r, ans)
