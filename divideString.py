d9e1c6003a1b7bcc97d9cf472ad314f3a623d994
sougata das <sougata19582001@gmail.com>
2022-11-14 18:02:31 +0530

    added comments and replaced the else block with break

--- divideString.py
+++ divideString.py
@@ -3,23 +3,36 @@ def divide_string(s: str, k: int, fill: str) -> list[str]:
     l = r = 0
     seperated_string = []
     while r < n:
+        # grouping the s by k
         group = ""
+
+        # if the r is greater than equal to n
+        # set the r to n
+        # if not then r = l + k
         if l + k < n:
             r = l + k
         else:
             r = n
 
+        # adding the char from l to r in the group.
         for i in range(l, r):
             if r == n and i == r-1:
+                # if in last group and k char a not present then
+                # its replacement will be fill
+                # if all are present then fill*(k - len(group)) == 0
+                # therefor no change will occurr.
                 group += s[i]
                 group += fill*(k - len(group))
-            else:
-                group += s[i]
+                break
 
+            group += s[i]
+
+        # shifting the l
         l = r
 
+        # append the group to the main list.
         seperated_string.append(group)
-    
+
     return seperated_string
 
 
