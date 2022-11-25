55e254942b178829619b4d2e286fdab1aa30a916
sougata das <sougata19582001@gmail.com>
2022-11-25 14:53:58 +0530

    deleted the old long solution with new consize one and a solution using xor

--- SingleNumber.py
+++ SingleNumber.py
@@ -1,30 +1,32 @@
 def single_number(nums):
-    single_num = None
     nums.sort()
-    if len(nums) > 1:
-        for i in range(len(nums)):
-            if i == 0 or i == len(nums)-1:
-                if i == 0:
-                    if nums[i] != nums[i+1]:
-                        single_num = nums[0]
-                        break
-                else:
-                    if nums[i] != nums[i-1]:
-                        single_num = nums[-1]
-                        break
-            else:
-                if nums[i] != nums[i+1] and nums[i] != nums[i-1]:
-                    single_num = nums[i]
-                    break
-    else:
-        if len(nums) == 0:
-            pass
+    num_count = 1
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1]:
+            num_count += 1
         else:
-            single_num = nums[0]
-    return single_num
+            if num_count == 1:
+                return nums[i-1]
+            else:
+                num_count = 1
+
+    if num_count == 1:
+        return nums[len(nums)-1]
+    return -1
+
+
+def single_number_bit_manipulation(nums):
+    xor = 0
+    for num in nums:
+        xor ^= num
+    return xor
 
 
 if __name__ == "__main__":
     print(single_number([4, 1, 2, 1, 2]))
     print(single_number([2, 2, 1]))
-    print(single_number([1, 2, 3, 4, 5]))
+    print(single_number([1]))
+    print()
+    print(single_number_bit_manipulation([4, 1, 2, 1, 2]))
+    print(single_number_bit_manipulation([2, 2, 1]))
+    print(single_number_bit_manipulation([1]))
